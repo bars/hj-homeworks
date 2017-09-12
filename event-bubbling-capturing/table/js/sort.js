91df6cca910a4b68c98c4373cf46e51762544330
Bars88 <barser88@gmail.com>
2017-09-13 01:33:50 +0300

    перенес решение

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,22 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  let target = event.target;
+  if (target.tagName === 'TH') {
+    if (target.dataset.dir !== undefined) {
+      target.dataset.dir = target.dataset.dir * (-1);
+    }
+    else {
+      target.dataset.dir = 1;
+    };
+    this.dataset.sortBy = target.getAttribute('data-prop-name');
+    sortTable(target.getAttribute('data-prop-name'), target.getAttribute('data-dir') )
+  }  
 }
+
+/********************************
+ * Не менять код ниже           *
+ ********************************/
+
+const table = document.querySelector('table');
+    table.addEventListener('click', handleTableClick);
